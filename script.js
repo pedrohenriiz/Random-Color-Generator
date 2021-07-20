6fc4cff0e14e191341743ae29a303264956d729b
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2021-07-20 19:04:53 -0300

    Feature/click-to-copy

--- script.js
+++ script.js
@@ -3,6 +3,18 @@
 const btn = document.querySelector('.btn');
 const color = document.querySelector('.color');
 const colorsGroup = document.querySelector('.colors-group');
+const pastColors = document.querySelectorAll('.past-colors');
+
+
+function clickToCopy(generatedColor) {
+  const createHiddenElement = document.createElement('textarea');
+  createHiddenElement.value = generatedColor;
+  document.body.appendChild(createHiddenElement);
+  createHiddenElement.select();
+  document.execCommand('copy');
+  document.body.removeChild(createHiddenElement);
+  alert(`The color ${generatedColor} was copied!`);
+} 
 
 function generatePastColorElement(generatedColor) {
   const colorsGroupChild = document.createElement('div');
@@ -11,6 +23,10 @@ function generatePastColorElement(generatedColor) {
   const pastColorElement = document.createElement('div');
   pastColorElement.classList.add('past-color');
   pastColorElement.style.backgroundColor = generatedColor;
+
+  pastColorElement.addEventListener('click', () => {
+    return clickToCopy(generatedColor)
+  } );
 
   const pastColorText = document.createElement('p');
   pastColorText.innerHTML = generatedColor;
